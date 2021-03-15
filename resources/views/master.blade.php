5667aecaecc0e1439054a88bb438d94f281aba18
oladayo ahmod <oladayoahmod112@gmail.com>
2021-03-14 17:28:18 -0700

    added the master file to hold the assets

---
+++ resources/views/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ url('assets/css/bootstrap.min.css')}}" type="text/css" rel="stylesheet">
+    <link href="{{ url('assets/css/style.css')}}" type="text/css" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="{{url('assets/bootstrap.min.css')}}"> --}}
+    <title>Ecommerce | Laravel</title>
+</head>
+<body>
+    {{-- {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}} --}}
+
+    <script src="{{ url('assets/js/jquery.min.js')}}"></script>
+    <script src="{{ url('assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{ url('assets/js/popper.min.js')}}"></script>
+</body>
+</html>
