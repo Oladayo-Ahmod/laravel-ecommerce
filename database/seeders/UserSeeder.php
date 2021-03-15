5991237c59a8f31c4189dba465838b9426a572ff
oladayo ahmod <oladayoahmod112@gmail.com>
2021-03-14 17:10:41 -0700

    seed the userseeder to the database

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use DB;
+use Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert(
+            [
+                'name'=> 'oladayo ahmod',
+                'email'=> 'oladayoahmod112@gmail.com',
+                'password'=> Hash::make('olami')
+            ]
+            );
     }
 }
