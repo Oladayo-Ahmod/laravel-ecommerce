05a6f3bbb07e618251b8d7b5d35deacdddee29c0
oladayo ahmod <oladayoahmod112@gmail.com>
2021-03-16 03:37:12 -0700

    make products seeder to the database

---
+++ database/seeders/ProductsSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'name'=> 'Monitor',
+            'price'=> 35000,
+            'category'=> 'desktop computer',
+            'description'=> 'a desktop computer',
+            'gallery' => 'monitor.jpg'
+        ],
+        [
+            'name'=> 'Home theatre',
+            'price'=> 2000,
+            'category'=> 'speaker',
+            'description'=> 'a sensible headset',
+            'gallery' => 'n3.jpg'
+        ],
+        [
+            'name'=> 'Headset',
+            'price'=> 1500,
+            'category'=> 'Headset',
+            'description'=> 'a awesome headset',
+            'gallery' => 'n3.png'
+        ],
+        [
+            'name'=> 'Graphics Image',
+            'price'=> 1300,
+            'category'=> 'graphics',
+            'description'=> 'a graphics design',
+            'gallery' => 'graphics.jpeg'
+        ]);
+    }
+}
