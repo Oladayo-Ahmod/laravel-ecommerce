6fa3b38f644b5dec613dd183ad10263fb5c2d871
oladayo ahmod <oladayoahmod112@gmail.com>
2021-05-03 03:45:20 -0700

    updated the login functionality of the  admin panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,9 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Admin;
+use Hash;
+use Session;
 class AdminController extends Controller
 {
     function login(Request $req){
-        return $req->input();
+        $req->validate([
+            'email' => 'required|email|max:100',
+            'password' => 'required|min:5'
+        ]);
+        if($admin = Admin::where('email','=',$req->email)->first()){
+            if(Hash::check($req->password,$admin->password)){
+                Session::put('admin',$admin);
+                return redirect('/dashboard');
+            }
+            else{
+                return back()->with('failure','incorrect email or password');
+            }
+        }
+        else{
+            return back()->with('failure','incorrect email or password');
+        }
+       
     }
 }
