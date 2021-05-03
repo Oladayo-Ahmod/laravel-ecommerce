6ca4ccd154b6a8a8d22c918d189d6187ed402cdd
oladayo ahmod <oladayoahmod112@gmail.com>
2021-05-02 18:05:57 -0700

    updated

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -32,7 +32,6 @@ class PaymentController extends Controller
     public function handleGatewayCallback()
     {
         $paymentDetails = Paystack::getPaymentData();
-
         dd($paymentDetails);
         // Now you have the payment details,
         // you can store the authorization_code in your db to allow for recurrent subscriptions
