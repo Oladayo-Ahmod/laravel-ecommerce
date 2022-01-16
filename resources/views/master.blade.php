b9f17fa4ab035041cc4cff0bb1dcea7bf88de22a
oladayo ahmod <oladayoahmod112@gmail.com>
2022-01-16 23:00:14 +0100

    update add to cart functionality

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ url('assets/css/bootstrap.min.css')}}" type="text/css" rel="stylesheet">
     <link href="{{ url('assets/css/style.css')}}" type="text/css" rel="stylesheet">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <link rel="stylesheet" href="{{ url('assets/css/sweetalert2.min.css') }}">
     {{-- <link rel="stylesheet" href="{{url('assets/bootstrap.min.css')}}"> --}}
     <title>Ecommerce | Laravel</title>
 </head>
@@ -15,7 +17,9 @@
     {{View::make('footer')}}
 
     <script src="{{ url('assets/js/jquery.min.js')}}"></script>
+    <script src="{{ url('assets/js/script.js')}}"></script>
     <script src="{{ url('assets/js/bootstrap.min.js')}}"></script>
     <script src="{{ url('assets/js/popper.min.js')}}"></script>
+    <script src="{{ url('assets/js/sweetalert2.min.js')}}"></script>
 </body>
 </html>
