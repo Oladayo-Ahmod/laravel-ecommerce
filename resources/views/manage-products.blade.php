482ac1c866e624525553a589cdb12b2bfb29c390
oladayo ahmod <oladayoahmod112@gmail.com>
2022-01-16 21:15:18 +0100

    modify products and categories

--- resources/views/manage-products.blade.php
+++ resources/views/manage-products.blade.php
@@ -41,66 +41,7 @@
             <!-- ============================================================== -->
             <!-- end pageheader  -->
             <!-- ============================================================== -->
-            <div class="ecommerce-widget">
-
-                <div class="row">
-                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="text-muted">Total Revenue</h5>
-                                <div class="metric-value d-inline-block">
-                                    <h1 class="mb-1">$12099</h1>
-                                </div>
-                                <div class="metric-label d-inline-block float-right text-success font-weight-bold">
-                                    <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
-                                </div>
-                            </div>
-                            <div id="sparkline-revenue"></div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="text-muted">Affiliate Revenue</h5>
-                                <div class="metric-value d-inline-block">
-                                    <h1 class="mb-1">$12099</h1>
-                                </div>
-                                <div class="metric-label d-inline-block float-right text-success font-weight-bold">
-                                    <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
-                                </div>
-                            </div>
-                            <div id="sparkline-revenue2"></div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="text-muted">Refunds</h5>
-                                <div class="metric-value d-inline-block">
-                                    <h1 class="mb-1">0.00</h1>
-                                </div>
-                                <div class="metric-label d-inline-block float-right text-primary font-weight-bold">
-                                    <span>N/A</span>
-                                </div>
-                            </div>
-                            <div id="sparkline-revenue3"></div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="text-muted">Avg. Revenue Per User</h5>
-                                <div class="metric-value d-inline-block">
-                                    <h1 class="mb-1">$28000</h1>
-                                </div>
-                                <div class="metric-label d-inline-block float-right text-secondary font-weight-bold">
-                                    <span>-2.00%</span>
-                                </div>
-                            </div>
-                            <div id="sparkline-revenue4"></div>
-                        </div>
-                    </div>
-                </div>
+            @include('layouts/order_details')             
                 <div class="row">
                     <!-- ============================================================== -->
               
