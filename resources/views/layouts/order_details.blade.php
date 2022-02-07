8c23c3ea0899b5d939a362656850af857ab3a5f2
oladayo ahmod <oladayoahmod112@gmail.com>
2022-02-07 01:41:24 +0100

    orders tables functionalities

--- resources/views/layouts/order_details.blade.php
+++ resources/views/layouts/order_details.blade.php
@@ -5,7 +5,7 @@
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="text-muted">Total Orders</h5>
+                    <h5 class="text-muted">Recent Orders</h5>
                     <div class="metric-value d-inline-block">
                         <h1 class="mb-1">{{$count_orders}}</h1>
                     </div>
