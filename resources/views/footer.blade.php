59d69694e68903eb0c595625a5e59970f78a21b8
Oladayo Ahmod <57647734+Oladayo-Ahmod@users.noreply.github.com>
2022-05-25 12:22:31 +0100

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class=" mt-5">
     <p>Copyright &copy; 2021 | Ecommerce in Laravel | Developed By 
-    <a href="https://unitechdev.com" class="text-danger">Unitech</a> 
+    <a href="https://oladayo-ahmod.github.io/portfolios/" class="text-danger">Oladayo Ahmod</a> 
     </p>
 </footer>
